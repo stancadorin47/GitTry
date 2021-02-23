18aed7e7dd255c3eecc317b771ff28739e14007f
owner <stancadorin47@gmail.com>
2021-02-23 16:25:24 +0200

    First try

--- src/GitTry.java
+++ src/GitTry.java
@@ -1,5 +1,5 @@
 public class GitTry {
     public static void main(String[] args) {
-        System.out.println("Let's try the second commit");
+        System.out.println("Let's try  second commit");
     }
 }
